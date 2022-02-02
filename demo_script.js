bde5def670c924507d87c24086b7d4c365b22c11
= <=>
2022-02-02 11:01:49 +0100

    Got clock working

--- demo_script.js
+++ demo_script.js
@@ -1,3 +1,16 @@
 function update_clock() {
-  document.getElementById("clock").innerHTML = "Le new";
+  const d = new Date();
+  let h = d.getHours();
+  let m = d.getMinutes();
+  let s = d.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  let clock = h + ":" + m + ":" + s;
+  document.getElementById("clock").innerHTML = clock;
+  setTimeout(update_clock,1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
 }
